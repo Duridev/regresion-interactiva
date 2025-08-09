12610041a870d3b241e069bc7ec23a8621027189
duridev <duridev@outlook.com>
2025-08-09 00:02:40 -0400

    entrad de hora y notas

--- modelo_predictivo.py
+++ modelo_predictivo.py
@@ -3,3 +3,13 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 
+print("Vas a construir tu primer modelo de preduccion con Python.")
+print("Ingresarás horas estudiasdas y la nota obtenida. Escribe 'fin para terminar'.\n")
+
+horas = []
+notas = []
+
+while  True:
+    entrada_hora = input("Horas de estudio (o 'fin'): ")
+    if entrada_hora.lower() == 'fin':
+        break
