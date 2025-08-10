1ac0c24b31f425654c01a3e2081669f3950fc510
duridev <duridev@outlook.com>
2025-08-09 23:08:13 -0400

    creacion ciclo while con peticion de notas y horas

--- modelo_predictivo.py
+++ modelo_predictivo.py
@@ -13,3 +13,14 @@ while  True:
     entrada_hora = input("Horas de estudio (o 'fin'): ")
     if entrada_hora.lower() == 'fin':
         break
+    entrada_nota = input("Nota obtenida: ")
+    try:
+        horas.append(float(entrada_hora))
+        notas.append(float(entrada_nota))
+    except ValueError:
+        print("Ingresa solo los números válidos")
+        continue
+    
+if len(horas) < 2:
+    print("Necesitas al menos dos registros para entrenar el modelo.")
+    exit()
